3532e2a7c7e5fc2b45627bd4859f3c782b118878
Temujin4480 <44020596+Temujin4480@users.noreply.github.com>
2019-05-10 08:08:48 +0700

    Add files via upload

---
+++ ClassAndMethodsAssignment/ClassAndMethodsAssignment/Math.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodsAssignment
+{
+    public class Math
+    {
+        public void CalculateMethod1(int x)
+        {
+            int Triple = x * 3;
+
+            Console.WriteLine("That integer multiplied by 3 is " + Triple + ".");
+            Console.ReadLine();
+        }
+
+        public void CalculateMethod2(int x)
+        {
+            int Quarter = x / 4;
+
+            Console.WriteLine("That integer divided by 4 is " + Quarter + ".");
+            Console.ReadLine();
+        }
+
+        public void CalculateMethod3(int x)
+        {
+            int Sixth = x / 6;
+
+            Console.WriteLine("That integer divided by 6 is " + Sixth + ".");
+            Console.ReadLine();
+        }
+    }
+
+    
+
+}
---
+++ ClassAndMethodsAssignment/ClassAndMethodsAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodsAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Please input an integer and hit enter.");
+            int userInteger = Convert.ToInt32(Console.ReadLine());
+
+            math.CalculateMethod1(userInteger);
+            math.CalculateMethod2(userInteger);
+            math.CalculateMethod3(userInteger);
+        }
+    }
+}
