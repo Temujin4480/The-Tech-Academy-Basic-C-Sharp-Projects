8ff4ce2faec4b0ddf489bb5ce0ce67fc09959e5d
Temujin4480 <44020596+Temujin4480@users.noreply.github.com>
2019-05-11 08:41:30 +0700

    Add files via upload

---
+++ MoreClassesAndMethods/MoreClassesAndMethods/IntDoubleString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreClassesAndMethods
+{
+    public class IntDoubleString
+    {
+        public void Calculate(int x)
+        {
+            int value1 = x * 50;
+            Console.WriteLine(value1);
+            Console.ReadLine();
+        }
+
+        public void Calculate(double y)
+        {
+            double value2 = y / 2.2;
+            Console.WriteLine(value2);
+            Console.ReadLine();
+        }
+
+        public void Calculate(string z)
+        {
+            int value3 = z.Length + 1;
+            Console.WriteLine(value3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MoreClassesAndMethods/MoreClassesAndMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreClassesAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntDoubleString keshik = new IntDoubleString();
+
+            int number1 = 12;
+
+            keshik.Calculate(x: number1);
+            Console.ReadLine();
+
+            double number2 = 198.5;
+
+            keshik.Calculate(y: number2);
+            Console.ReadLine();
+
+            string name = "Ben";
+
+            keshik.Calculate(z: name);
+            Console.ReadLine();
+        }
+    }
+}
