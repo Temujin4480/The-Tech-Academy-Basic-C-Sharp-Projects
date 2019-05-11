38a9c6dcf87e5e994f4bec682cbb9d15b273f79e
Temujin4480 <44020596+Temujin4480@users.noreply.github.com>
2019-05-11 08:08:58 +0700

    Add files via upload

---
+++ TwoIntClassMethod/TwoIntClassMethod/Math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoIntClassMethod
+{
+    public class Math//Create a class
+    {
+        public void Calculate1(int x, int y)//create a void method that takes two integers as parameters. 
+        {
+            int value1 = x * 4;
+            int value2 = y;
+            Console.WriteLine("The first integer 5, multiplied by 4 is: " + value1 + ". There was also a " + value2 + ".");
+            //Have the method do a math operation on the first integer and display the second integer to the screen.
+            Console.ReadLine();
+        }
+
+
+      
+      
+    }
+}
---
+++ TwoIntClassMethod/TwoIntClassMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoIntClassMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();//In the Main() method of the console app, instantiate the class.
+            //Console.WriteLine(math);
+            //Console.ReadLine();
+
+
+            int x = 5;
+            int y = 10;
+
+            math.Calculate1(x, y);//Call the method in the class, passing in two numbers.
+            Console.ReadLine();
+            
+
+            //math = Calculate1(First, Second);
+
+        
+        }
+    }
+}
