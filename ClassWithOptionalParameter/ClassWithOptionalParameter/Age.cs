dd191a617958a623c76d750393362665730d1b2a
Temujin4480 <44020596+Temujin4480@users.noreply.github.com>
2019-05-15 01:04:29 +0700

    Add files via upload

---
+++ ClassWithOptionalParameter/ClassWithOptionalParameter/Age.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWithOptionalParameter
+{
+    public class Age
+    {
+
+        public int Calculate(int x, int y = 0)
+        {
+            int ageCombined = x + y;
+
+            Console.WriteLine("Your age and your first child's age is " + ageCombined + ".");
+            //Console.WriteLine("Your age and your first child's age is {0}", ageCombined + "."); //another way to write the same thing
+            Console.ReadLine();//These lines have to be put above the return
+            return ageCombined;
+        }
+    }
+
+    
+}
---
+++ ClassWithOptionalParameter/ClassWithOptionalParameter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWithOptionalParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Age age = new Age();
+            int result;
+
+
+            Console.WriteLine("Enter your age:");
+            int age1 = Convert.ToInt32(Console.ReadLine());
+            //if (!int.TryParse(Console.ReadLine(), out age1))
+            //    throw new ArgumentException("no valid number");
+            Console.WriteLine("Enter the age of your first child.  If you don't have a child press enter:");
+            string age2 = Console.ReadLine();
+            int age3;
+
+            if (string.IsNullOrEmpty(age2))
+            {
+                result = age.Calculate(age1);
+            }
+
+            else
+            {
+                age3 = Convert.ToInt32(age2);
+                result = age.Calculate(age1, age3);
+            }
+
+
+            //age.Calculate(age1, age3);
+            Console.ReadLine();
+
+        }
+    }
+}
