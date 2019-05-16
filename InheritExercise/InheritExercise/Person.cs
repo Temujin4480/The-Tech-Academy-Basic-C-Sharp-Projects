d5c4a93bb1b0538929034b936783d24f7382baa2
Temujin4480 <44020596+Temujin4480@users.noreply.github.com>
2019-05-16 12:56:32 +0700

    Add files via upload

---
+++ InheritExercise/InheritExercise/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritExercise
+{
+    public class Person
+    {
+        //Add a list here and make the method SayName use a foreach loop? 
+        //public List<string> FullNames { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Full Name: " + FirstName + " " + LastName);
+        }
+    }
+}
