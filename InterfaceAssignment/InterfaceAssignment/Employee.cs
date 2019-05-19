d48ab1edc1ea9b198c76ee2b6f58d7d4cd2e0535
Temujin4480 <44020596+Temujin4480@users.noreply.github.com>
2019-05-19 07:31:10 +0700

    Add files via upload

---
+++ InterfaceAssignment/InterfaceAssignment/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAssignment
+{
+    public class Employee : Person, IQuittable
+    {
+
+        public override void SayName()
+        {
+            Console.WriteLine("Full Name: " + FirstName + " " + LastName);
+        }
+
+        public List<string> employees1 { get; set; }
+
+        public void Quit() 
+        {
+            Console.WriteLine("Everyone is fired");
+
+            //My original code below wasn't working, I believe because it was returning a valid and was not void.  
+            //foreach (string employeee in employees1)
+            //{
+            //    if (employeee.Contains("q"))
+            //    {
+            //        Console.WriteLine(employeee + " Employment Status: No longer employed");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(employeee + " Employment Status: Currently employed");
+            //    }
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ InterfaceAssignment/InterfaceAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Create an interface called IQuittable and have it define a void method called Quit().
+
+            //2. Have your Employee class from the previous drill inherit that interface and implement the Quit() 
+            //method in any way you choose.
+
+            //3. Use polymorphism to create an object of type IQuittable and call the Quit() method on it. 
+            //Hint: an object can be of an interface type if it implements that specific interface.
+
+            Employee employee = new Employee();
+            IQuittable employees1 = new Employee(); //Employee morphs into the superclass/interface Quittable
+            List<string> employees = new List<string>(); //if I write it this way I can add to it
+            //List<IQuittable> employees = new List<IQuittable>();
+            employees.Add("Odell Beckham");
+            employees.Add("Jarvis Landry");
+            employees.Add("Tariq Black");
+
+            employee.Quit();
+            Console.ReadLine();
+
+
+            //Following method works with my original Quit method, but don't think it follows the criteria of part 3 of the assignment:
+            //Employee employee = new Employee();
+            //employee.employees = new List<string>() { "Odell Beckham", "Jarvis Landry", "Tariq Black" };
+
+            //employee.Quit(employee);
+            //Console.ReadLine();
+        }
+    }
+}
